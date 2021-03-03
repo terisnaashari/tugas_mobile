da7f79e184b669ffc335b40e42363498ba537731
terisnaashari <78597329+terisnaashari@users.noreply.github.com>
2021-03-03 23:20:17 +0700

    Create variabel.dart

---
+++ variabel.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Membuat variabel
+  String nama = "Daeng Mhd El Faritsi";
+  int umur = 21;
+  double tinggi = 175;
+  bool isMenikah = false;
+
+  // varibel dengan kata kunci var
+  var alamat = "Lubuk Pakam";
+
+  // Cetak variabel
+  print("Nama saya $nama, Umur $umur tahun, Tinggi sekitar $tinggi cm.");
+  print("Menikah: $isMenikah");
+  print("Alamat: $alamat");
+  
+}
